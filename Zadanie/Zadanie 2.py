f37d0f68196845087e223ef5bec93c8830de29d1
AgataSlusarczyk <aga.slu.24@gmail.com>
2025-05-20 21:24:45 +0200

    Usunięte rozwiązanie zadania 2

--- Zadanie/Zadanie 2.py
+++ Zadanie/Zadanie 2.py
@@ -11,24 +11,19 @@ import time
 def measure_time(func):
     def wrapper(*args, **kwargs):
         # TODO: Zapisz aktualny czas przed wywołaniem funkcji.
-        start = time.time()
 
         # TODO: Wywołaj oryginalną funkcję 'func' z przekazanymi argumentami i zapisz jej wynik.
-        result = func(*args, **kwargs)
 
         # TODO: Zapisz aktualny czas po wywołaniu funkcji.
-        end = time.time()
 
         # TODO: Oblicz różnicę między 'end' a 'start', aby uzyskać czas wykonania.
-        duration = end - start
 
         # TODO: Wypisz czas wykonania w konsoli, używając formatu z przykładu.
         # Użyj nazwy funkcji i zmierzonego czasu .
         # Sformatuj czas do 6 miejsc po przecinku.
-        print(f"Czas wykonania {func.__name__}: {duration:.6f} s")
 
         # TODO: Zwróć wynik oryginalnej funkcji
-        return result
+
     return wrapper
 
 @measure_time
